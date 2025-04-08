aecbbbbe9c3a179d9ea0aa30e400a92fc8a918ef
euler <eullerborgesdamotta155@gmail.com>
2025-04-08 17:14:59 -0300

    Initial commit

---
+++ src/test/java/com/project/grpc/GrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
