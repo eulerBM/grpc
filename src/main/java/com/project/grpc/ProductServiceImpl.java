75fbbef097150b048c774c4998173e28486aec7a
euler <eullerborgesdamotta155@gmail.com>
2025-04-14 15:09:56 -0300

    changeProduct done

--- src/main/java/com/project/grpc/ProductServiceImpl.java
+++ src/main/java/com/project/grpc/ProductServiceImpl.java
@@ -1,16 +1,14 @@
 package com.project.grpc;
 
-import com.example.grpc.IdRequest;
-import com.example.grpc.ProductServiceGrpc;
-import com.example.grpc.idProductRequest;
+import com.example.grpc.*;
 import com.project.grpc.Entity.ProductEntity;
 import com.project.grpc.Repository.ProductRepository;
 
-import com.example.grpc.CreateProductRequest;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.util.List;
 import java.util.Optional;
 
 @GrpcService
@@ -64,6 +62,63 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
                 .build();
 
         responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void changeProduct(ProductRequest request, StreamObserver<IdRequest> responseObserver) {
+
+        Optional<ProductEntity> productById = productRepository.findById(request.getId());
+
+        if (productById.isEmpty()) {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("Produto com ID " + request.getId() + " não encontrado.")
+                            .asRuntimeException()
+            );
+            return;
+        }
+
+        ProductEntity product = productById.get();
+
+        IdRequest response = IdRequest.newBuilder()
+                .setId(product.getId())
+                .setName(product.getName())
+                .setDescription(product.getDescription())
+                .setPrice(product.getPrice().floatValue())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllProducts(nameProductRequest request, StreamObserver<ProductListResponse> responseObserver) {
+
+        List<ProductEntity> productById = productRepository.findByNameContainingIgnoreCase(request.getName());
+
+        if (productById.isEmpty()) {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("Produto com nome " + request.getName() + " não encontrado.")
+                            .asRuntimeException()
+            );
+            return;
+        }
+
+        ProductListResponse.Builder responseBuilder = ProductListResponse.newBuilder();
+
+        for (ProductEntity product : productById) {
+            IdRequest productProto = IdRequest.newBuilder()
+                    .setId(product.getId())
+                    .setName(product.getName())
+                    .setDescription(product.getDescription())
+                    .setPrice(product.getPrice().floatValue())
+                    .build();
+
+            responseBuilder.addProducts(productProto);
+        }
+        responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
     }
 }
--- src/main/java/com/project/grpc/Repository/ProductRepository.java
+++ src/main/java/com/project/grpc/Repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.project.grpc.Repository;
 import com.project.grpc.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findByNameContainingIgnoreCase(String name);
 }
